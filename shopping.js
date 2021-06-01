bdc21e4aca7467171965310542c37276bdc572a1
KippMinton <luciankippminton@gmail.com>
2021-06-01 15:42:04 -0500

    created addToShoppingList function

--- shopping.js
+++ shopping.js
@@ -1,4 +1,4 @@
-const groceries = [
+let groceries = [
     {
         id: 1,
         name: "kale",
@@ -30,5 +30,48 @@ const groceries = [
         price: 5.99
     }
 ]
+
+const addToShoppingList = (groceryObj, arr) => {
+   let arrLength = arr.length;
+   if (arrLength === 0){
+      groceryObj.id = 1;
+   } else {
+      let lastID = arr[arrLength - 1].id;
+      groceryObj.id = lastID + 1;
+   }
+   groceryObj.dateCreated = new Date().toLocaleDateString("en-US").split("/") + "," + new Date().toLocaleTimeString("en-US").split(/:| /)
+   arr.push(groceryObj);
+}
+
+let sprouts = {
+   name: "Brussels sprouts", 
+   price: 4.00
+}
+
+let water5 = {
+   name: "water: 5gal",
+   price: 5.00
+}
+
+let tamales = {
+   name: "frozen tamales",
+   price: "7.00"
+}
+
+let catFood = {
+   name: "dry cat food",
+   price: 12.00
+}
+
+let chickenBreast = {
+   name: "chicken breast",
+   price: "4.99"
+}
+
+addToShoppingList(sprouts, groceries)
+addToShoppingList(water5, groceries)
+addToShoppingList(tamales, groceries)
+addToShoppingList(catFood, groceries)
+addToShoppingList(chickenBreast, groceries)
 
 console.log(groceries)
